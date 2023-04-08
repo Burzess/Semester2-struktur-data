9ba0a4d33d422d980c474d05475dca90c0352550
Julaeha <103539851+Burzess@users.noreply.github.com>
2023-04-09 05:39:54 +0700

    Update dosen.go

--- Tugas_UTS/model/dosen/dosen.go
+++ Tugas_UTS/model/dosen/dosen.go
@@ -86,34 +86,21 @@ func GetList(g *entities.GerbongDsn) {
 }
 
 func GetListByNip(g *entities.GerbongDsn, nip string) {
-	// Deklarasi variabel data sebagai slice of slice yang akan digunakan sebagai data tabel
-	data := [][]string{}
-	// Deklarasi variabel curGerbong sebagai pointer ke struct GerbongDsn
-	curGerbong := g
-	// Looping untuk mencari dosen dengan NIP yang sesuai dengan parameter nip pada setiap gerbong
-	for curGerbong.Next != nil {
-		if curGerbong.Next.Dosen.Nip == nip {
-			// Jika dosen dengan NIP yang sesuai ditemukan, tambahkan informasinya pada variabel data
-			data = append(data, []string{curGerbong.Dosen.Nip, curGerbong.Dosen.Nama})
-			break
-		}
-		curGerbong = curGerbong.Next
-	}
+	current := g
 
-	if len(data) == 0 { // Jika tidak ditemukan dosen dengan NIP yang sesuai, tampilkan pesan error
-		fmt.Println("Dosen dengan NIP", nip, "tidak ditemukan.")
-	} else { // Jika ditemukan, tampilkan informasinya dalam bentuk tabel
-		// Deklarasi variabel table sebagai tablewriter yang akan digunakan untuk menghasilkan output dalam bentuk tabel
-		table := tablewriter.NewWriter(os.Stdout)
-		// Mengatur header tabel
-		table.SetHeader([]string{"NIP", "Nama"})
-		// Mengatur border tabel
-		table.SetBorder(false)
-		// Mengatur alignment isi data pada tabel ke kiri
-		table.SetAlignment(tablewriter.ALIGN_LEFT)
-		// Menambahkan data pada tabel
-		table.AppendBulk(data)
-		// Mengatur output dari tabel ke console
-		table.Render()
+	for current.Next != nil {
+		if current.Next.Dosen.Nip == nip {
+			dsn := current.Next.Dosen
+			// Inisialisasi tabel dan header kolom
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"NIP", "Nama"})
+			row := []string{dsn.Nip, dsn.Nama}
+			table.Append(row)
+			// Render tabel ke terminal
+			table.Render()
+			return
+		}
+		current = current.Next
 	}
+	fmt.Printf("Data dengan NIP %s tidak tersedia", nip)
 }
